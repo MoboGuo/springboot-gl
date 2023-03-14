3253eda3567937c9b834795c217fe18605601a76
MoboGuo <GUOmb960113@163.com>
2023-03-14 17:33:19 +0800

    初始提交

---
+++ gl-admin/src/main/java/com/example/gladmin/GlAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.gladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlAdminApplication.class, args);
+    }
+
+}
