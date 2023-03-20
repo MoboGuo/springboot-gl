1aecd1059156bf2899f2dc0e1570ad196a35108f
lkle <1446244720@qq.com>
2023-03-20 21:36:21 +0800

    整合Sa-Token，添加登录验证页

--- gl-admin/src/main/java/com/example/gladmin/GlAdminApplication.java
+++ gl-admin/src/main/java/com/example/gladmin/GlAdminApplication.java
@@ -1,5 +1,6 @@
 package com.example.gladmin;
 
+import cn.dev33.satoken.SaManager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ public class GlAdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GlAdminApplication.class, args);
+        System.out.println("启动成功：Sa-Token配置如下：" + SaManager.getConfig());
     }
 
 }
